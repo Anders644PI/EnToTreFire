98d3d487bce40ce8371eb1929c569a89dbcc5b14
Anders644PI <anders.bruun.severinsen@gmail.com>
2023-08-10 10:56:32 +0200

    Added WeatherAction

--- app/src/main/java/eu/mistcloud/template/java/App.java
+++ app/src/main/java/eu/mistcloud/template/java/App.java
@@ -5,26 +5,24 @@ import org.json.JSONObject;
 import eu.mistcloud.tools.java.Mist;
 import eu.mistcloud.template.java.businesslogic.Weather;
 import eu.mistcloud.template.java.businesslogic.Danish;
+import eu.mistcloud.template.java.businesslogic.Weather;
 
 public class App {
 
-    static void handleWeatherAction(byte[] payloadBytes, JSONObject envelope) {
-        // Optionally, we can use the envelope to access event metadata
-        String messageId = envelope.getString("messageId");
-        String traceId = envelope.getString("traceId");
-
-        // We can access the event body by converting it to a string
-        Weather.handleWeatherGreeting(new String(payloadBytes));
-    }
-
     static void handleDanishAction(byte[] payloadBytes, JSONObject envelope) {
         Danish.handleDanishGreeting(new String(payloadBytes));
+    }
+
+    static void handleWeatherAction(byte[] payloadBytes, JSONObject envelope) {
+        // We can access the event body by converting it to a string
+        Weather.handleWeatherGreeting(new String(payloadBytes));
     }
 
     public static void main(String[] args) {
         Mist.service(args)
                 .handle("weatherAction", App::handleWeatherAction)
                 .handle("danishAction", App::handleDanishAction)
+                .handle("weatherAction", App::handleWeatherAction)
                 .init(() -> System.out.println("Init!"));
     }
 }
